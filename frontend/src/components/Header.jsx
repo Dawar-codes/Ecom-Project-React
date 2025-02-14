7b1fb346c0925ffcf9fe65f625ebbd28ac00a25b
Dawar-codes <dawar.malik29@gmail.com>
2025-02-14 15:02:39 +0500

    Updated search input with mobile hidden feature

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -97,7 +97,7 @@ export default function Header() {
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
               placeholder="Search..."
-              className="px-4 py-2 rounded-lg bg-zinc-700 text-zinc-200 placeholder-zinc-400 
+              className="hidden md:block px-4 py-2 rounded-lg bg-zinc-700 text-zinc-200 placeholder-zinc-400 
                          focus:outline-none focus:ring-[1.3px] focus:ring-leather-500 focus:border-transparent transition"
             />
           
